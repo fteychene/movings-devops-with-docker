c435328babe0e0bc454e4494b33025910b58113b
fteychene <francois.teychene@gmail.com>
2016-06-01 14:42:07 +0200

    Fix prez problem.

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo \> mvn clean package
-mvn clean package
-
 read
+mvn clean package
 
 echo
 echo
 echo
 echo \> docker build -t fteychene/jee:dockerfile .
+read
 docker build -t fteychene/jee:dockerfile .
 
-read
 
 echo
 echo
 echo
 echo \> docker run -p 8080:8080 fteychene/jee:dockerfile
+read
 docker run -p 8080:8080 fteychene/jee:dockerfile
